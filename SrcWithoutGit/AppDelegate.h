dd3890be7ed516ec5760df36b1f06309c95e888d
Eileen <eileenelement@gmail.com>
2013-12-19 10:37:11 +0800

    Initial Commit

---
+++ SrcWithoutGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SrcWithoutGit
+//
+//  Created by Eileen on 18/12/13.
+//  Copyright (c) 2013 Eileen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
